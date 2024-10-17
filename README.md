e2479803471045b0db07c905af537ca9d8c0fc24
kunkun <540610423@qq.com>
2024-10-17 23:00:19 +0800

    feat: add config abount pm2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 351Robot 通讯模块
 本模块采用Nest框架作为服务端框架，同时使用FoxgloveBridge第三方库与ROS进行通信。
+
+## 启动与部署
+```
+npm install pm2 -g
+npm run build
+```
---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name: "foxglove-robot-middleware",
+    script: "dist/main.js",
+    autorestart: true,
+    env: {
+      "NODE_ENV": "development",
+    }
+  }]
+}
