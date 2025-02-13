a32819e4913d4aafa113cf34643830de0605b342
kunkun <540610423@qq.com>
2025-02-13 19:49:35 +0800

    test: deepseek module

---
+++ src/module/deepseek/deepseek.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { DeepseekController } from './deepseek/deepseek.controller';
+import { DeepseekService } from './deepseek/deepseek.service';
+
+@Module({
+  controllers: [DeepseekController],
+  providers: [DeepseekService]
+})
+export class DeepseekModule {}
---
+++ src/module/deepseek/deepseek.ts
@@ -0,0 +1,3 @@
+export class UserMessageDto {
+  userMessage: string
+}
