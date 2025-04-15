6a9645c75a9e85798eb3621d353b1b77d28cc47f
kunnwei <540610423@qq.com>
2025-04-15 11:11:01 +0800

    refactor: remove deepseek

--- src/module/deepseek/deepseek.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeepseekController } from './deepseek/deepseek.controller';
-import { DeepseekService } from './deepseek/deepseek.service';
-
-@Module({
-  controllers: [DeepseekController],
-  providers: [DeepseekService]
-})
-export class DeepseekModule {}
--- src/module/deepseek/deepseek.ts
+++
@@ -1,3 +0,0 @@
-export class UserMessageDto {
-  userMessage: string
-}
