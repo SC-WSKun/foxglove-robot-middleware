795344fbe6eef938b67dfc9a5eb4da5b98c01cd1
kunnwei <540610423@qq.com>
2025-04-16 22:37:07 +0800

    feat: remove robot-mcp-server

--- src/module/robot/robot.module.ts
+++ src/module/robot/robot.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { RobotController } from './robot.controller';
-import { RobotService } from './robot.service';
+import { Module } from '@nestjs/common'
+import { RobotController } from './robot.controller'
+import { RobotService } from './robot.service'
+import { ConfigModule } from '@nestjs/config'
+import { resolve } from 'path'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [() => require(resolve('src/config/qwen.json'))],
+    }),
+  ],
   controllers: [RobotController],
-  providers: [RobotService]
+  providers: [RobotService],
 })
 export class RobotModule {}
--- src/module/robot/robot.service.ts
+++ src/module/robot/robot.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import MCPClient from 'src/utils/McpClient'
 
 @Injectable()
-export class RobotService {}
+export class RobotService {
+  mcpClient: MCPClient
+  constructor() {
+    this.mcpClient = new MCPClient()
+    this.mcpClient.connectToServer('src/utils/mcp-server.js')
+  }
+}
